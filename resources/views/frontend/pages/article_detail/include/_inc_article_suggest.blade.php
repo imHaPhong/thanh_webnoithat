5de3845ee66b3a44fcf55992e8bea19378352607
imHaPhong <64822110+imHaPhong@users.noreply.github.com>
2021-04-27 21:55:22 +0700

    first commit

---
+++ resources/views/frontend/pages/article_detail/include/_inc_article_suggest.blade.php
@@ -0,0 +1,7 @@
+<div class="post-detail_suggest" style="margin: 10px 0">
+    <h3 >Bài viết liên quan</h3>
+    <ul>
+        <li><a href=""><span ></span> </a></li>
+        
+    </ul>
+</div>
---
+++ resources/views/frontend/pages/article_detail/include/_inc_content.blade.php
@@ -0,0 +1,46 @@
+<h2 class="sapo">Ngày 14/6/2015 vừa qua Công ty cổ phần trực tuyến Đăng Quang vinh dự được trao chứng nhận “ Thương hiệu, sản phẩm và dịch vụ xuất sắc năm 2014 – 2015” </h2>
+
+<div class="noidung">
+    <p>
+        <a href="https://dantri.com.vn/thi-truong/dang-quang-watch-duoc-vinh-danh-trong-gala-bao-chi-voi-doanh-nhan-doanh-nghiep-2015080707545808.htm" target="_blank">&gt;&gt; Link chi tiết xem tại Báo Dân trí</a>
+    </p>
+
+    <p>Sáng ngày 14/6/2015, Lễ Vinh Danh “Thương hiệu – Sản Phẩm – Dịch Vụ xuất sắc năm 2014 -2015” đã long trọng diễn ra tại Nhà Hát Lớn, Hà Nội.&nbsp;Chương trình diễn ra nhằm vinh danh những doanh nhân, doanh nghiệp tiêu biểu năm 2014 - 2015. Đồng thời trao bảng vàng lưu danh cho "Doanh nhân, doanh nghiệp, Nhà quản lý và Nhà hoạt động xã hội tiêu biểu thời hội nhập 2014 - 2015. Gala cũng dành nguồn quỹ trao học bổng cho sinh viên nghèo vượt khó, học giỏi.</p>
+
+    <p style="text-align:center">
+        <a href="https://www.dangquangwatch.vn/lib/ckfinder/images/IMG_3639(1).jpg" class="example-image-link countimg0" data-lightbox="example-1"><img alt="" src="https://www.dangquangwatch.vn/lib/ckfinder/images/IMG_3639(1).jpg" style="height:333px; width:500px" data-lightbox="roadtrip"></a>
+    </p>
+
+    <p></p>
+
+    <p style="text-align:center">
+        <a href="https://www.dangquangwatch.vn/lib/ckfinder/images/IMG_9655s(1).jpg" class="example-image-link countimg1" data-lightbox="example-1"><img alt="" src="https://www.dangquangwatch.vn/lib/ckfinder/images/IMG_9655s(1).jpg" style="height:533px; width:500px" data-lightbox="roadtrip"></a>
+    </p>
+
+    <p></p>
+
+    <p style="text-align:center">
+        <a class="example-image-link countimg2" href="https://dangquangwatch.vn/lib/ckfinder/images/IMG_9678s.jpg" data-lightbox="example-1"><img alt="" src="https://dangquangwatch.vn/lib/ckfinder/images/IMG_9678s.jpg" style="height:308px; line-height:20.7999992370605px; text-align:center; width:500px" data-lightbox="roadtrip"></a>
+    </p>
+
+    <p></p>
+
+    <p style="text-align:center">
+        <a href="https://www.dangquangwatch.vn/lib/ckfinder/images/DSC_1342(1).JPG" class="example-image-link countimg3" data-lightbox="example-1"><img alt="Đăng Quang Watch được vinh danh trong Gala “Báo chí với doanh nhân, doanh nghiệp”" src="https://www.dangquangwatch.vn/lib/ckfinder/images/DSC_1342(1).JPG" style="height:355px; width:500px" data-lightbox="roadtrip"></a>
+    </p>
+
+    <p style="text-align:center"></p>
+
+    <p>
+        <a href="http://www.dangquangwatch.vn"></a></p>
+
+    <p>&nbsp;</p>
+
+</div>
+<div class="cb"></div>
+<div class="other" style="padding-top: 20px;">
+    <p class="title">Bài viết khác</p>
+    <ul>
+
+    </ul>
+</div>
